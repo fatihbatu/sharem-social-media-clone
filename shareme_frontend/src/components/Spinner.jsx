e44d151d581e650c891a072a539dd7eb16d1eeed
fatihbatu <77248966+fatihbatu@users.noreply.github.com>
2023-03-08 17:50:23 +0300

    Spinner css changes submitted

--- shareme_frontend/src/components/Spinner.jsx
+++ shareme_frontend/src/components/Spinner.jsx
@@ -3,7 +3,7 @@ import { Circles } from 'react-loader-spinner';
 
 const Spinner = ({ message }) => {
   return (
-    <div className='flex flex-col justify-center items-center w-full h-full'>
+    <div className='flex flex-col items-center justify-center h-screen'>
       <Circles color='#00BFFF' height={50} width={200} className='m-5' />
       <p className='text-lg text-center px-2'>{message}</p>
     </div>
